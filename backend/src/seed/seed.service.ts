58bc470de4399cdf4e4d1c90a234ad54f6720b33
manuelMarkDenver <manuel.markdenver@gmail.com>
2025-08-20 02:17:49 +0800

    fixed ts errors in the backend // BOTH WORKING

--- backend/src/seed/seed.service.ts
+++ backend/src/seed/seed.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { PrismaService } from '../prisma/prisma.service'
+import { PrismaService } from '../core/prisma/prisma.service'
 import { execSync } from 'child_process'
 
 @Injectable()
@@ -41,7 +41,7 @@ export class SeedService {
         this.prisma.tenant.count(),
         this.prisma.user.count({ where: { role: 'GYM_MEMBER' } }),
         this.prisma.plan.count(),
-        this.prisma.gymMembershipPlan.count(),
+        this.prisma.membershipPlan.count(),
         this.prisma.user.count({ where: { email: 'admin@creatives-saas.com' } })
       ])
 
