b27c70ea2135620a89bde51b899417cde15b2aa3
manuelMarkDenver <manuel.markdenver@gmail.com>
2025-10-27 03:30:37 +0800

    analytics ts issue fixed

--- backend/src/modules/gym/analytics/gym-analytics.module.ts
+++ backend/src/modules/gym/analytics/gym-analytics.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GymAnalyticsController } from './gym-analytics.controller';
 import { GymAnalyticsService } from './gym-analytics.service';
 import { PrismaModule } from '../../../core/prisma/prisma.module';
+import { SupabaseModule } from '../../../core/supabase/supabase.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SupabaseModule],
   controllers: [GymAnalyticsController],
   providers: [GymAnalyticsService],
   exports: [GymAnalyticsService],
