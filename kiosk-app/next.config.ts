60cca3504841dbaf12581feb7a9f968df6606297
manuelMarkDenver <manuel.markdenver@gmail.com>
2026-01-20 23:57:43 +0800

    updated next.config.js

--- kiosk-app/next.config.ts
+++ kiosk-app/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/v1/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'}/api/v1/:path*`,
       },
     ];
   },
